3951b15baf4e72d025aab416b8894ed029a9997c
JakubT <91053990+arezik@users.noreply.github.com>
2026-02-27 09:29:40 +0100

    zadania

--- main.c
+++ main.c
@@ -3,14 +3,40 @@
 #include <stdbool.h>
 
 int main()
-{
-    int liczbaCalkowita = 125;
-    int liczba2 = 9999;
-    int liczba3;
+{ /*
+    int liczbaCalkowita;
+    printf ("Podaj liczbe calkowita: \n");
+    scanf ("%d", &liczbaCalkowita);
+    printf ("%d", liczbaCalkowita);
 
-    char napis[25];
-    printf ("Podaj \\napis (max 24 znaki): ");
-    scanf("%24s", napis);
-    printf ("%s", napis);
-    return 0;
+    float licz1;
+    float licz2;
+    printf ("Podaj dwie liczby: ");
+    scanf ("%f" , &licz1);
+    scanf ("%f" , &licz2);
+    float roz = licz1 - licz2;
+    printf("roznica tych liczb to: %f", roz);
+
+    int rokur;
+    printf ("Podaj rok urodzenia: ");
+    scanf ("%d", &rokur);
+    printf ("%d", rokur-1);
+
+    int licz1;
+    int licz2;
+    int licz3;
+    printf ("Podaj 3 liczby calkowite: ");
+    scanf ("%d %d %d", &licz1, &licz2, &licz3);
+    float srednia = (licz1 + licz2 + licz3) / 3 ;
+    printf ("Srednia to: %f", srednia);
+       */
+
+    char znak;
+    char znakdwa;
+    printf ("podaj 1 znak: ");
+    scanf ("%s", &znak);
+    printf ("Podaj 2 znak: ");
+    scanf ("%s", &znakdwa);
+    printf ("%s, ", &znakdwa);
+    printf ("%s", &znak);
 }
