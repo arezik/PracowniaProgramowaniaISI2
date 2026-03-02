db2792042508b86ddd28d214fb9e037c45ae5443
JakubT <91053990+arezik@users.noreply.github.com>
2026-03-02 11:12:34 +0100

    praca na zajeciach

---
+++ 02.03.26/main.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{/*
+    double x1 = 0.1 + 0.2;
+    double x2 = 0.3;
+    double epsilon = 1e-9;
+    if (fabs(x1 - x2) < epsilon) {
+        printf("Prawid³owy wynik \n");
+    }else if (fabs(x1-x2 == epsilon)) {
+        printf("roznica jest rowna dokladnie 0.000000000000001 \n");
+    }else{printf("Nieprawidlowy wynik");
+    }
+//zadanie 1 Operatory Arytmetyczne
+
+int a =23;
+int b =6;
+int c =5;
+printf("%i", (a+b)%c);
+
+
+//zadanie 2
+
+double a = 10, b = 4;
+printf("%f", pow(a,2)-pow(b,2));
+    return 0;
+
+//zadanie 3
+
+
+int a = 10;
+int b = 7;
+int c = 2;
+printf("%i", (a-b)*c);
+
+//zadanie 4
+
+double a, b, c;
+printf ("Podaj 3 liczby (format 'a, b, c'): ");
+scanf("%lf, %lf, %lf", &a, &b, &c);
+printf("Wynik sredniej geometrycznej to: %lf", pow(a*b*c, 1.0/3.0));
+
+//zadanie 5
+
+double a, b;
+printf ("Podaj 2 liczby ca³kowite (format a, b): ");
+scanf ("%lf, %lf", &a, &b);
+printf ("n\Kwadrat sumy tych liczb to: %lf", pow((a+b), 2.0));
+
+//zadanie 6
+
+double a, b, c;
+printf ("Podaj 3 liczby calkowite (Format'a, b, c')");
+scanf ("%lf, %lf, %lf", &a, &b, &c);
+printf ("%lf", pow(a,2.0)+ pow(b,2.0)+ pow(c,2.0));
+
+
+
+// Instrukcje warunkowe, operator warunkowy
+//zadanie 1.
+
+int a;
+printf("Podaj liczbe calkowita:");
+scanf ("%i", &a);
+printf ("\n");
+if (a > 0){
+    printf("liczba jest wieksza od 0");
+}else if (a < 0){
+    printf("Liczba jest mniejsza od 0");
+}else if (a == 0) {
+    printf("Liczba jest rowna 0");
+}
+
+//zadanie 2
+int a, b;
+printf("Podaj 2 liczby calkowite (Format 'a, b'): ");
+scanf ("%i, %i", &a, &b);
+if (a > b) {
+    printf("\n %i jest wieksze od %i", a, b);
+} else if (b > a) {
+    printf("\n %i jest wieksze od %i", b, a);
+} else if (a == b) {
+    printf ("\n Obie liczby s¹ sobie rowne");
+}
+*/
+
+
+
+
+
+}
